6521651b2983e4192908ce84db7f93d4289c1144
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2015-01-27 15:12:26 -0800

    added bioguideid to senators model so that we can pull new data from a different API

--- accountability_app/db/schema.rb
+++ accountability_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124184256) do
+ActiveRecord::Schema.define(version: 20150127230823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150124184256) do
     t.string   "osid"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "bioguideid"
   end
 
   create_table "users", force: :cascade do |t|
