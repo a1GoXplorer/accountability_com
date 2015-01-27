332e53367c20a366b7ed0668b1b8011c40e9f656
Samuel Cabral <samfcabral@gmail.com>
2015-01-27 14:53:43 -0800

    new layout set with senators and bills in nav bar. Footer added but not styled

--- accountability_app/config/routes.rb
+++ accountability_app/config/routes.rb
@@ -15,11 +15,15 @@ Rails.application.routes.draw do
 
   get 'sites/index'
 
+  get 'about', to: 'sites#about'
+
   get 'sites/about'
 
   get 'sites/contact'
 
   get 'sessions/new'
+
+  get 'senators', to: 'senators#index'
 
   get 'senators/index'
 
@@ -33,3 +37,32 @@ Rails.application.routes.draw do
 
 
 end
+
+#         Prefix Verb   URI Pattern               Controller#Action
+#    bills_index GET    /bills/index(.:format)    bills#index
+#           root GET    /                         sites#index
+#          users GET    /users(.:format)          users#index
+#                POST   /users(.:format)          users#create
+#      edit_user GET    /users/:id/edit(.:format) users#edit
+#           user GET    /users/:id(.:format)      users#show
+#                PATCH  /users/:id(.:format)      users#update
+#                PUT    /users/:id(.:format)      users#update
+#                DELETE /users/:id(.:format)      users#destroy
+#        sign_up GET    /sign_up(.:format)        users#new
+#        profile GET    /profile(.:format)        users#profile
+#          login GET    /login(.:format)          sessions#new
+#                POST   /login(.:format)          sessions#create
+#        session DELETE /session(.:format)        sessions#destroy
+#         logout GET    /logout(.:format)         sessions#destroy
+#    sites_index GET    /sites/index(.:format)    sites#index
+#          about GET    /about(.:format)          sites#about
+#    sites_about GET    /sites/about(.:format)    sites#about
+#  sites_contact GET    /sites/contact(.:format)  sites#contact
+#   sessions_new GET    /sessions/new(.:format)   sessions#new
+#       senators GET    /senators(.:format)       senators#index
+# senators_index GET    /senators/index(.:format) senators#index
+#        senator GET    /senators/:id(.:format)   senators#show
+#   senators_new GET    /senators/new(.:format)   senators#new
+#  senators_edit GET    /senators/edit(.:format)  senators#edit
+#          bills GET    /bills(.:format)          bills#index
+
